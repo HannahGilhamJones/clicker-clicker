87144818ee00ee0c5f33c332dd1cfd26a22c93db
Hannah Milton <hannahmilton@live.co.uk>
2018-02-15 19:25:36 +0000

    skeleton work for resolution modification

--- ClickerClicker/settingsmodel.cpp
+++ ClickerClicker/settingsmodel.cpp
@@ -1,6 +1,9 @@
 #include "settingsmodel.h"
 
-SettingsModel::SettingsModel(QObject *parent)
+#include <QApplication>
+#include <QWidget>
+
+SettingsModel::SettingsModel(QObject * parent)
     : QObject(parent),
       m_volume(100),
       m_fullScreenState(true)
@@ -18,6 +21,11 @@ bool SettingsModel::getFullScreenState()
     return m_fullScreenState;
 }
 
+QSize SettingsModel::getResolution()
+{
+    return m_resolution;
+}
+
 void SettingsModel::setVolume(int volume)
 {
     if(volume != m_volume)
@@ -32,6 +40,16 @@ void SettingsModel::setFullScreenState(bool fullScreenState)
     if(fullScreenState != m_fullScreenState)
     {
         m_fullScreenState = fullScreenState;
+
         emit this->fullScreenStateChanged(m_fullScreenState);
+    }
+}
+
+void SettingsModel::setResolution(QSize resolution)
+{
+    if(m_resolution != resolution)
+    {
+        m_resolution = resolution;
+        emit this->resolutionChanged(m_resolution);
     }
 }
--- ClickerClicker/settingsmodel.h
+++ ClickerClicker/settingsmodel.h
@@ -10,6 +10,7 @@ class SettingsModel : public QObject
 
     Q_PROPERTY(int volume READ getVolume WRITE setVolume NOTIFY volumeChanged)
     Q_PROPERTY(bool fullscreenState READ getFullScreenState WRITE setFullScreenState NOTIFY fullScreenStateChanged)
+    Q_PROPERTY(QSize resolution READ getResolution WRITE setResolution NOTIFY resolutionChanged)
 
 public:
 
@@ -17,21 +18,24 @@ public:
 
     int             getVolume();
     bool            getFullScreenState();
+    QSize           getResolution();
 
 signals:
 
     void            volumeChanged(int volume);
     void            fullScreenStateChanged(bool fullScreenState);
+    void            resolutionChanged(QSize resolution);
 
 public slots:
 
     void            setVolume(int volume);
     void            setFullScreenState(bool fullScreenState);
+    void            setResolution(QSize resolution);
 
 private:
 
     int             m_volume;
-    QPair<int, int> m_resolution;
+    QSize           m_resolution;
     bool            m_fullScreenState;
 };
 
