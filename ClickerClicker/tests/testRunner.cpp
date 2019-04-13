8ccaf436cea728752f5499f55240352e5480adb5
Hannah Milton <hannahmilton@live.co.uk>
2019-04-13 13:32:15 +0100

    Fix Test Runner so that all tests run together

--- ClickerClicker/tests/testRunner.cpp
+++ ClickerClicker/tests/testRunner.cpp
@@ -1,19 +1,34 @@
 #include <QTest>
 
-#include "testgamebuttonmodel.cpp"
-#include "testboostbuttonmodel.cpp"
+#include "tst_boostbuttonmodelsuite.h"
+#include "tst_gamebuttonmodelsuite.h"
+#include "tst_gamemanagersuite.h"
+#include "tst_gametimersuite.h"
+#include "tst_settingssuite.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[])
+{
     int status = 0;
-
     {
-        TestGameButtonModel testA;
-        status |= QTest::qExec(&testA, argc, argv);
+        BoostButtonModelSuite suite;
+        status |= QTest::qExec(&suite, argc, argv);
+
     }
-
     {
-        TestBoostButtonModel testB;
-        status |= QTest::qExec(&testB, argc, argv);
+        GameButtonModelSuite suite;
+        status |= QTest::qExec(&suite, argc, argv);
+    }
+    {
+        GameManagerSuite suite;
+        status |= QTest::qExec(&suite, argc, argv);
+    }
+    {
+        GameTimerSuite suite;
+        status |= QTest::qExec(&suite, argc, argv);
+    }
+    {
+        SettingsSuite suite;
+        status |= QTest::qExec(&suite, argc, argv);
     }
 
     return status;
